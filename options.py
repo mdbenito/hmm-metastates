e9f0cc2fd82d44a11e698df9d1ab8128e8de6378
Miguel de Benito Delgado <nonick@8027.org>
2015-11-20 21:26:37 +0100

    Fix silly name conflict

--- options.py
+++ options.py
@@ -11,37 +11,37 @@ def parse(argv):
                 '\t-h, --help\t\tThis help\n' + \
                 '\t-i, --input-file\tSpecifies the input file\n' + \
                 '\t-o, --output-file\tSpecifies the output file [default=input-file.out]\n' + \
-                '\t-j, --jobs\t\tNumber of concurrent jobs to launch [default={}]\n'.format(opts.jobs) + \
-                '\t-t, --trials\t\tSet number of trials for input file [default={}]\n'.format(opts.trials) + \
-                '\t-s, --states\t\tNumber of states to test [default={}]\n'.format(opts.states) + \
+                '\t-j, --jobs\t\tNumber of concurrent jobs to launch [default={}]\n'.format(vals.jobs) + \
+                '\t-t, --trials\t\tSet number of trials for input file [default={}]\n'.format(vals.trials) + \
+                '\t-s, --states\t\tNumber of states to test [default={}]\n'.format(vals.states) + \
                 '\t-a, --auto\t\tParse input filename for number of trials and maximal K\n' + \
                 '\t\t\t\t(Overrides -s, -t)' + \
                 '\t-v, --verbose\tDisplay progress indicators'
     try:
-        opts, args = getopt.getopt(argv, 'hi:o:j:t:s:a:v',
+        vals, args = getopt.getopt(argv, 'hi:o:j:t:s:a:v',
                                    ['help', 'input-file=', 'output-file=', 'jobs=', 'trials=', 'states=', 'auto=',
                                     'verbose'])
     except getopt.GetoptError:
         print(usage_str)
         sys.exit(2)
-    for opt, arg in opts:
+    for opt, arg in vals:
         if opt in ('-h', '--help'):
             print(usage_str)
             sys.exit()
         elif opt in ('-i', '--input-file'):
-            opts.input_file = arg
+            vals.input_file = arg
         elif opt in ('-o', '--output-file'):
-            opts.output_file = arg
+            vals.output_file = arg
         elif opt in ('-j', '--jobs'):
-            opts.jobs = int(arg)
+            vals.jobs = int(arg)
         elif opt in ('-t', '--trials'):
-            opts.trials = int(arg)
+            vals.trials = int(arg)
         elif opt in ('-s', '--states'):
-            opts.states = [int(arg)]  # TODO: parse list of integers
+            vals.states = [int(arg)]  # TODO: parse list of integers
         elif opt in ('-a', '--auto'):
-            opts.auto = True
+            vals.auto = True
         elif opt in ('-v', '--verbose'):
-            opts.verbose = True
+            vals.verbose = True
     if not opts.input_file:
         print(usage_str)
         sys.exit(1)
