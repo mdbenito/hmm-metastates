c3a04c6be98d0f6c90cde4c3700c6f4ffc9437a8
Miguel de Benito Delgado <nonick@8027.org>
2015-12-30 23:07:33 +0100

    Fix some regressions and a bit of formatting. Workaround problem in ribbon() with some versions of matplotlib

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ def compute_lengths(series, num_trials):
     return trial_length * np.ones(num_trials)
 
 
-def infer(series, lengths, n_states=2, viterbi=True, verbose=False, start=0, end=0, n_iter=10, tol=5e-1):
+def infer(series, lengths, n_states=2, viterbi=True, verbose=False, start=0, end=0, n_iter=2000,
+          tol=1e-6):
     """
 
     Parameters
@@ -74,9 +75,10 @@ def infer(series, lengths, n_states=2, viterbi=True, verbose=False, start=0, end
 
     """
     outputs = np.unique(series)
-    if verbose: print('Inferring parameters of MultinomialHMM for n_states={0}, max. iterations={1}, tolerance={2}'.
-                      format(n_states, n_iter, tol))
-    m = hmm.MultinomialHMM(n_components=n_states, n_iter=n_iter, tol=tol, verbose=verbose, algorithm='viterbi')
+    if verbose: print("Inferring parameters of MultinomialHMM for n_states={0}, "
+                      "max. iterations={1}, tolerance={2}".format(n_states, n_iter, tol))
+    m = hmm.MultinomialHMM(n_components=n_states, n_iter=n_iter, tol=tol, verbose=verbose,
+                           algorithm='viterbi')
     m.n_features = outputs.size
 
     tick = t.time()
@@ -85,10 +87,10 @@ def infer(series, lengths, n_states=2, viterbi=True, verbose=False, start=0, end
     tick = t.time()
     viterbi_path = []
     if viterbi:
-        viterbi_path = m.predict(series[:, None], lengths=lengths)
+        viterbi_path = m.predict(series.reshape((series.size, 1)), lengths=lengths)
         if verbose: print('Time for viterbi: {}s'.format(t.time() - tick))
 
-    return [[m, start, end], viterbi_path] # HACK HACK
+    return [[m, start, end], viterbi_path]  # HACK HACK
 
 
 # Alv's plotting
@@ -106,10 +108,12 @@ def plot(series, viterbi_path, sr=250):
 
     """
     df = viz.states(viterbi_path, sr, offset=0)
-    ax = viz.ribbon(df, sr, col.cyclic_col(col.cm1.colors), y0=0, y_width=0.1999, group_col='state')
-
-    viz.ribbon(viz.states(series, sr, offset=0), sr, col.cyclic_col(col.cm3.colors), y0=0.2, y_width=0.1999,
-               group_col='state', ax=ax)
+    # ax = viz.ribbon(df, sr, col.cyclic_col(col.cm1.colors), y0=0, y_width=0.1999, group_col='state')
+    ax = viz.ribbon(df, sr, col.stdcolors(), y0=0, y_width=0.1999, group_col='state')
+    # viz.ribbon(viz.states(series, sr, offset=0), sr, col.cyclic_col(col.cm3.colors), y0=0.2, y_width=0.1999,
+    #            group_col='state', ax=ax)
+    viz.ribbon(viz.states(series, sr, offset=0), sr, col.stdcolors(), y0=0.2, y_width=0.1999,
+                group_col='state', ax=ax)
     pl.show()
 
 
@@ -120,7 +124,7 @@ def cross_validate(series, opts):
       Trains with one subset of trials then tests against the N-1 remaining (computes the log likelihood / score).
     Scores are averaged over all N training/test stages
     """
-    if opts.verbose: print('Performing cross-validation with {} folds'.format(opts.nfold))
+    if opts.verbose: print("Performing cross-validation with {} folds".format(opts.nfold))
     scores = {k: 0 for k in opts.states}
     trial_length = compute_lengths(series, opts.trials)[1]  # HACK
     # trial_length = int(series.size / opts.trials)
@@ -137,11 +141,13 @@ def cross_validate(series, opts):
                 end = min(off+l, opts.trials)
                 off = end
                 training_trials = start + opts.trials-end
-                training = view[range(0, start)+range(end, opts.trials)].reshape(((start+opts.trials-end)*trial_length, 1))
+                training = view[range(0, start) +
+                                range(end, opts.trials)].reshape(((start + opts.trials - end) *
+                                                                  trial_length, 1))
                 training_lengths = compute_lengths(training, training_trials)
-                #if opts.verbose: print('\tFitting for fold #{}'.format(int(np.ceil(off/l))))
-                futures.append(ex.submit(infer, training, training_lengths,
-                                         n_states=k, viterbi=False, verbose=opts.verbose, start=start, end=end))
+                # if opts.verbose: print('\tFitting for fold #{}'.format(int(np.ceil(off/l))))
+                futures.append(ex.submit(infer, training, lengths=training_lengths, n_states=k,
+                                         viterbi=False, verbose=opts.verbose, start=start, end=end))
                 # m, _ = infer(training, training_lengths, n_states=k, viterbi=False, verbose=opts.verbose)
 
     for fut in as_completed(futures):
@@ -149,7 +155,7 @@ def cross_validate(series, opts):
         k = m.n_components
         testing = view[start:end].reshape(((end-start)*trial_length, 1))
         scores[k] += m.score(testing)
-        if opts.verbose: print('\tScore for fold {} - {}: {}'.format(start, end, scores[k]))
+        if opts.verbose: print("\tScore for fold {} - {}: {}".format(start, end, scores[k]))
     for k in scores.keys():
         scores[k] /= np.ceil(opts.trials/l)  # Average score over the number of splits
     return scores
@@ -160,9 +166,13 @@ def cross_validate(series, opts):
 def main(argv):
     np.random.seed(42)
     opts = options.parse(argv)
-    if opts.verbose: print('Loading file {}'.format(opts.input_file))
+    if opts.verbose:
+        print("Loading file {}".format(opts.input_file))
     series = load_observations(opts.input_file)
-    if opts.verbose: print('Working with n_states={0}, trials={1}, jobs={2}'.format(opts.states, opts.trials, opts.jobs))
+    lengths = compute_lengths(series, opts.trials)
+    if opts.verbose:
+        print("Working with n_states={0}, trials={1}, jobs={2}"
+              .format(opts.states, opts.trials, opts.jobs))
 
     if opts.nfold:
         scores = cross_validate(series, opts)
@@ -170,11 +180,13 @@ def main(argv):
     else:
         with ProcessPoolExecutor(max_workers=opts.jobs) as ex:
             n = len(opts.states)
-            for _, vpath in ex.map(infer,[series[:, None]] * n, opts.states, [opts.trials] * n, [True] * n,
-                                   [opts.verbose] * n):
+            for [m, _, _], vpath in ex.map(infer, [series[:, None]] * n, [lengths] * n, opts.states,
+                                   [True] * n, [opts.verbose] * n):
                 if opts.output_file:
                     save_viterbi(vpath, opts.output_file)
-                plot(series, vpath)
+                # plot(series, vpath)
+                print("Transition matrix:\n{}".format(np.round(m.transmat_, 2)))
+                print("Initial probability:\n{}".format(np.round(m.startprob_, 2)))
 
 
 if __name__ == '__main__':
